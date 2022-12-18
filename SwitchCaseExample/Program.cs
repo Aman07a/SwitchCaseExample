fd236f81919362bf6fe620027b95480d9f0e3cdb
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 09:41:26 +0100

    Switch-Case

--- SwitchCaseExample/Program.cs
+++ SwitchCaseExample/Program.cs
@@ -3,9 +3,26 @@ namespace SwitchCaseExample
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			//
+			// Variable to store grade letter of student
+			char gradeLetter = 'A';
+
+			// Find out description based on gradeLetter
+			string gradeDescription;
+
+			switch(gradeLetter)
+			{
+				case 'O': gradeDescription = "Outstanding"; break;
+				case 'A': gradeDescription= "Excellent"; break;
+				case 'B': gradeDescription = "Good"; break;
+				case 'C': gradeDescription = "Average"; break;
+				case 'F': gradeDescription = "Fail"; break;
+				default: gradeDescription = "None"; break;
+			}
+
+			System.Console.WriteLine(gradeDescription);
+			System.Console.ReadKey();
 		}
 	}
 }
